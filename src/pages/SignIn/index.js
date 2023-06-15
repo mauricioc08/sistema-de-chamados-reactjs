b89b23a4a2c220753ffa0de31f1597c0def609fe
mauricioc08 <mauricio-cassiano@hotmail.com>
2023-06-15 13:26:13 -0300

    create page and style of Login

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import "./signin.css";
+import logo from "../../assets/logo.png";
+import { Link } from "react-router-dom";
+
+const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className="containerCenter">
+      <div className="login">
+        <div className="loginArea">
+          <img src={logo} alt="Logo do sistema de chamado" />
+        </div>
+
+        <form>
+          <h1>Entrar</h1>
+          <input
+            type="text"
+            placeholder="email@email.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="******"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <button type="submit" value="Acessar">
+            Acessar
+          </button>
+        </form>
+
+        <Link to="/register">Criar uma Conta</Link>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
